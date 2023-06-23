2a0f00068f6a4b38e38a309464818e7334cb4cdf
Oscar <osw282@gmail.com>
2023-06-23 15:23:58 +0100

    Implementation of the mind data scraper for 2 sections

--- steps/scrape_mind_data/scrape_mind_data_step.py
+++ steps/scrape_mind_data/scrape_mind_data_step.py
@@ -1,6 +1,211 @@
 """Scrape data from the Mind charity website."""
+from datetime import date
+from typing import Any, Dict, List, Optional
+
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession  # type: ignore
+from zenml.logger import get_logger
 from zenml.steps import step
+
+logger = get_logger(__name__)
+
+
+class BaseScraper:
+    """Base data scraper class."""
+
+    mind_url = "https://www.mind.org.uk"
+    session = HTMLSession()
+
+    def get_html_text(self, url: str) -> str:
+        """Retrieve the HTML text content of a webpage.
+
+        Args:
+            url (str): The URL of the webpage.
+
+        Returns:
+            str: The HTML text content of the webpage.
+        """
+        return self.session.get(url).text  # type: ignore
+
+    def create_soup(self, url: str) -> BeautifulSoup:
+        """Create a BeautifulSoup object from the HTML text content of a webpage.
+
+        Args:
+            url (str): The URL of the webpage.
+
+        Returns:
+            BeautifulSoup: A BeautifulSoup object representing the webpage's HTML structure.
+        """
+        html_text = self.get_html_text(url)
+        soup = BeautifulSoup(html_text, "lxml")
+
+        return soup
+
+    def build_subpage_url(self, sub_page_url: str) -> str:
+        """Build a reachable URL for a subpage given its directory.
+
+        Args:
+            sub_page_url (str): The directory of the subpage.
+
+        Returns:
+            str: The complete URL for the subpage.
+        """
+        return str(self.mind_url + sub_page_url)
+
+
+class Scraper(BaseScraper):
+    """This class inherits the essential methods for data scraping from the BaseScraper class.
+
+    This class aims to scrape data from the "Types of mental health problems", "Drugs and treatments" and the
+    sub-sections from the "Helping someone else" as they all share the same HTML structure under.
+
+    Args:
+        BaseScraper (_type_): _description_
+    """
+
+    def extract_section_list(self, url: str) -> Dict[str, str]:
+        """Extracts a dictionary of object names and their corresponding sub-URLs.
+
+        E.g. From the types of mental health problems page. We would expect a dictionary of {"Anger": "url_to_anger_page"}
+
+        Args:
+            url (str): the url of a section from the information and support page such "Types of mental health problems" page.
+
+        Returns:
+             Dict[str, str]: A dictionary where the keys are object names and the values are their sub-URLs.
+        """
+        soup = self.create_soup(url)
+
+        conditions: Dict[str, str] = {}
+
+        for div in soup.find_all(
+            "div", class_="content-area bg-white"
+        ):  # Where the list starts
+            for a_tag in div.find_all("a"):
+                condition_name = a_tag.get_text(strip=True)
+                href = a_tag["href"]
+
+                if (
+                    condition_name == "Yes"
+                ):  # The class content-area bg-white contains some non related text
+                    return conditions
+
+                conditions[condition_name] = href
+
+        return conditions
+
+    def get_object_side_bar_urls(
+        self, url: str, side_bar_url_to_exclude: Optional[str]
+    ) -> List[str]:
+        """This function should return the side bar urls for each object found with the extract_section_list() function.
+
+        Args:
+            url (str): object's url. E.g. The URL to the anger condition page or URL to the Antidepressants page.
+            side_bar_url_to_exclude (Optional[str]): sidebar url to exclude if needed. E.g. Antidepressants contains URL Antidepressants A–Z which is also an object on its own.
+
+
+        Returns:
+            List[str]: A list of side bar urls of an object such as a condition or a drug.
+        """
+        side_bar_urls = []
+
+        soup = self.create_soup(self.build_subpage_url(url))
+
+        side_bar_list = soup.find(
+            "ul", attrs={"class": "sidebar-menu", "id": "sidebar_menu"}
+        )
+
+        if side_bar_list:
+            for li_tag in side_bar_list.find_all("li"):  # type: ignore
+                a_tag = li_tag.find("a")
+                href = a_tag["href"]
+                side_bar_urls.append(href)
+
+            if side_bar_url_to_exclude:
+                side_bar_urls = [
+                    url
+                    for url in side_bar_urls
+                    if not url.endswith(side_bar_url_to_exclude)
+                ]
+
+        return side_bar_urls
+
+    def scrape_sub_page_data(self, sub_page_url: str, content_class: str) -> List[str]:
+        """This function scrapes data given a page URL and the class name of where contents are.
+
+        Args:
+            sub_page_url (str): The URL of the sub-page to scrape.
+            content_class (str): The class name of the elements containing the desired content.
+
+        Returns:
+            List[str]: A list of strings containing the scraped data.
+        """
+        sub_page_data = []
+
+        soup = self.create_soup(self.build_subpage_url(sub_page_url))
+
+        # Contents for pages with a sidebar are stored in the col-md-8 class, and for pages without a sidebar, contents are stored in the col-md-12 class.
+        content_divs = soup.find_all("div", class_=content_class)
+
+        for div in content_divs:
+            heading = div.find(["h2", "h3"])
+            sub_page_texts = div.find_all(["p", "li"])
+
+            if heading is not None:
+                sub_page_data.append(heading.text)
+
+            for sub_page_text in sub_page_texts:
+                sub_page_data.append(sub_page_text.text)
+
+        return sub_page_data
+
+    def get_archived_url(self, url: str, time_stamp: str) -> str:
+        """Retrieve the archived URL of a webpage snapshot using the Wayback Machine API.
+
+        Args:
+            url (str): The URL of the webpage.
+            time_stamp (str): The timestamp of the desired snapshot.
+
+        Returns:
+            str: The archived URL of the snapshot.
+        """
+        api_link = (
+            f"http://archive.org/wayback/available?url={url}&timestamp={time_stamp}"
+        )
+
+        answer = requests.get(api_link)
+        json_answer: Dict[str, Any] = answer.json()
+
+        if bool(json_answer["archived_snapshots"]):  # Whether the URL is archived.
+            archived_url = json_answer["archived_snapshots"]["closest"]["url"]
+        else:
+            return "URL not archived"
+
+        return archived_url  # type: ignore
+
+    def create_dataframe(self, data: Dict[str, str]) -> pd.DataFrame:
+        """Create a pandas DataFrame from the given data.
+
+        Args:
+            data (dict): A dictionary containing URL and TextScraped pairs.
+
+        Returns:
+            pd.DataFrame: The created DataFrame with columns TextScraped, TimeStamp, URL, and ArchivedURL.
+        """
+        date_of_today = date.today().strftime("%Y%m%d")
+        df = pd.DataFrame(data.items(), columns=["URL", "TextScraped"])
+
+        # Using WayBack Machine API to obtain a archived URL if exists
+        df["ArchivedURL"] = df.apply(
+            lambda row: self.get_archived_url(row["URL"], date_of_today), axis=1
+        )
+        df["TimeStamp"] = date_of_today
+
+        df = df[["TextScraped", "TimeStamp", "URL", "ArchivedURL"]]  # Rearrange Columns
+
+        return df
 
 
 @step
@@ -10,6 +215,29 @@ def scrape_mind_data() -> pd.DataFrame:
     Returns:
         pd.DataFrame: data scraped.
     """
-    df = pd.DataFrame()
+    url = "https://www.mind.org.uk/information-support/types-of-mental-health-problems/"
+    scraper = Scraper()
+
+    objects = scraper.extract_section_list(url)
+
+    logger.info(f"Found {len(objects)} objects from {url}")
+    data = {}
+
+    for obj, url in objects.items():
+        logger.info(f"Scraping data for the {obj} object")
+        sub_page_urls = scraper.get_object_side_bar_urls(url, "a-z/")
+        column_class = "col-md-8 column" if sub_page_urls else "col-md-12 column"
+
+        for sub_page_url in sub_page_urls:
+            sub_page_data = scraper.scrape_sub_page_data(sub_page_url, column_class)
+            sub_page_data_string = "\n".join(sub_page_data)
+            full_url = scraper.build_subpage_url(sub_page_url)
+            data[full_url] = sub_page_data_string
+
+    logger.info("Finish scraping data")
+
+    logger.info("Creating dataframe")
+    df = scraper.create_dataframe(data)
+    print(df.head())
 
     return df
