b7a3ac22be77ee0e77769a767a03ad6fb693e106
Chris <chris@fuzzylabs.ai>
2023-06-26 14:55:09 +0100

    Add mock for getting Azure connection string from Terraform outputs

--- tests/test_steps/test_save_data_step/test_save_data_step.py
+++ tests/test_steps/test_save_data_step/test_save_data_step.py
@@ -18,13 +18,28 @@ def mock_blob_service() -> BlobServiceClient:
         yield mock_blob_service
 
 
+@pytest.fixture
+def mock_get_azure_connection_string() -> str:
+    """Fixture for mocked Azure Storage connection string.
+
+    Yields:
+        str: Mock connection string
+    """
+    with patch(
+        "steps.save_data.save_data_step.get_azure_connection_string"
+    ) as get_azure_connection_string:
+        get_azure_connection_string.return_value = "azure-connection-string"
+        yield get_azure_connection_string
+
+
 def test_azure_upload_df(
-    mock_blob_service: BlobServiceClient,
+    mock_blob_service: BlobServiceClient, mock_get_azure_connection_string: str
 ):
     """Test AzureStorage upload file function.
 
     Args:
         mock_blob_service (BlobServiceClient): Mocked blob service client
+        mock_get_azure_connection_string (str): Mocked Azure Storage connection string
     """
     # Mock container client
     mock_container_client = mock_blob_service.from_connection_string.return_value
@@ -33,7 +48,7 @@ def test_azure_upload_df(
     mock_blob_client = mock_container_client.get_blob_client.return_value
 
     df = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
-    azure_upload_df("scraped-data-store", df, "nhs_data", "nhs")
+    azure_upload_df("scraped-data-store", df, "data", "nhs")
 
     # Check if upload_blob function is called exactly once with the data
     mock_blob_client.upload_blob.assert_called_once_with(
