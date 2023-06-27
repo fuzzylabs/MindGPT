03a098f91a323eb55b2ffb6fbd1f8f336b625aa3
KirsoppJ <40233184+KirsoppJ@users.noreply.github.com>
2023-06-27 12:23:05 +0100

    [MIN-8] create data preparation pipeline (#7)
    * framework
    
    * add yaml, add load_data step
    
    * add to run
    
    * init
    
    * fixing type complaints.
    
    * fix return on dpp
    
    * addressing comments.
    
    * steps folder restructure
    
    * sorting imports
    
    * generalise save_data slightly
    
    * make new save_data to avoid ambiguous typing
    
    * blank line
    
    * remove redundant file
    
    * add new steps to yaml

---
+++ pipelines/data_preparation_pipeline/data_preparation_pipeline.py
@@ -0,0 +1,29 @@
+"""Data preparation pipeline."""
+
+from steps.save_data.save_data_step import save_data
+from zenml.pipelines import pipeline
+from zenml.logger import get_logger
+from zenml.steps import BaseStep
+
+logger = get_logger(__name__)
+
+
+@pipeline
+def data_preparation_pipeline(
+    load_data: BaseStep,
+    clean_data: BaseStep,
+    validate_data: BaseStep,
+    save_prepared_data: BaseStep,
+) -> None:
+    """The data preparation pipeline.
+
+    Args:
+        load_data: A ZenML step which loads the data.
+        clean_data: A ZenML step which cleans the data.
+        validate_data: A ZenML step which validates the cleaned data.
+        version_data: A ZenML step which versions the cleaned and validated data.
+    """
+    data = load_data()
+    data = clean_data(data)
+    data = validate_data(data)
+    save_prepared_data(data)
