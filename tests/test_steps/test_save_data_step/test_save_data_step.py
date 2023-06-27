f1752516e572e990e720bc0c57e4baac640065e6
Chris <chris@fuzzylabs.ai>
2023-06-27 12:02:56 +0100

    Move mock data to fixture and use across tests

--- tests/test_steps/test_save_data_step/test_save_data_step.py
+++ tests/test_steps/test_save_data_step/test_save_data_step.py
@@ -48,55 +48,57 @@ def mock_blob_client() -> BlobClient:
 
 @pytest.fixture()
 def nhs_dataframe() -> pd.DataFrame:
-    """_summary_.
+    """NHS mock dataframe.
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: Mock dataframe
     """
     return pd.DataFrame({"NHS1": [1, 2], "NHS2": [3, 4]})
 
 
 @pytest.fixture()
 def mind_dataframe() -> pd.DataFrame:
-    """_summary_.
+    """Mind mock dataframe.
 
     Returns:
-        pd.DataFrame: _description_
+        pd.DataFrame: Mock dataframe
     """
     return pd.DataFrame({"MIND1": [100, 200], "MIND2": [300, 400]})
 
 
-def test_azure_upload_df(mock_blob_client: BlobClient):
+def test_azure_upload_df(mock_blob_client: BlobClient, nhs_dataframe: pd.DataFrame):
     """Test AzureStorage upload file function.
 
     Args:
         mock_blob_client (BlobClient): Mocked blob client
-        mock_get_azure_connection_string (str): Mocked Azure Storage connection string
+        nhs_dataframe (pd.DataFrame): Mocked "NHS" data in dataframe
     """
-    df = pd.DataFrame({"col1": [1, 2], "col2": [3, 4]})
-    azure_upload_df("scraped-data-store", df, "data", "nhs")
+    azure_upload_df("scraped-data-store", nhs_dataframe, "data", "nhs")
 
     # Check if upload_blob function is called exactly once with the data
     mock_blob_client.upload_blob.assert_called_once_with(
-        "col1,col2\n1,3\n2,4\n", blob_type="BlockBlob"
+        "NHS1,NHS2\n1,3\n2,4\n", blob_type="BlockBlob"
     )
 
 
-def test_save_data_step(mock_blob_client: BlobClient):
+def test_save_data_step(
+    mock_blob_client: BlobClient,
+    nhs_dataframe: pd.DataFrame,
+    mind_dataframe: pd.DataFrame,
+):
     """Test save data step works as expected.
 
     Args:
         mock_blob_client (BlobClient): Mocked blob client
-        mock_get_azure_connection_string (str): Mocked Azure Storage connection string
+        nhs_dataframe (pd.DataFrame): Mocked "NHS" data in dataframe
+        mind_dataframe (pd.DataFrame): Mocked "Mind" data in dataframe
     """
-    nhs_df = pd.DataFrame({"NHS1": [1, 2], "NHS2": [3, 4]})
-    mind_df = pd.DataFrame({"MIND1": [100, 200], "MIND2": [300, 400]})
-
     with does_not_raise():
-        save_data.entrypoint(nhs_df, mind_df, SaveDataParameters())
+        save_data.entrypoint(nhs_dataframe, mind_dataframe, SaveDataParameters())
 
     calls = [
         call("NHS1,NHS2\n1,3\n2,4\n", blob_type="BlockBlob"),
         call("MIND1,MIND2\n100,300\n200,400\n", blob_type="BlockBlob"),
     ]
+
     mock_blob_client.upload_blob.assert_has_calls(calls)
