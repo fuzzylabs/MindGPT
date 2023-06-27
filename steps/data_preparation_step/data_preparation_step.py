f9953026063a034066eebcf55faa8336b8ce8a48
Josh <josh.kirsopp@gmail.com>
2023-06-27 10:57:54 +0100

    steps folder restructure

--- steps/data_preparation_step/data_preparation_step.py
+++ steps/data_preparation_step/data_preparation_step.py
@@ -1,18 +1,1 @@
-from zenml.steps import step
-import pandas as pd
-
-
-@step
-def load_data() -> pd.DataFrame:
-    return pd.DataFrame()
-
-
-@step
-def clean_data(data: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame()
-
-
-@step
-def validate_data(data: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame()
 
