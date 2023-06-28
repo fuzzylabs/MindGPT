4500a0994c0ddbca64bef4cca8a560a15c9de180
Chris <chris@fuzzylabs.ai>
2023-06-28 10:34:38 +0100

    Update save_data step to give option to save data locally

--- steps/save_data/save_data_step.py
+++ steps/save_data/save_data_step.py
@@ -1,6 +1,7 @@
 """Save the scraped data as CSV and push to bucket."""
 import os
 from datetime import datetime
+from enum import Enum
 
 import pandas as pd
 from azure.storage.blob import BlobServiceClient
@@ -9,13 +10,28 @@ from utils.terraform_utils import TerraformVariables
 from zenml.steps import BaseParameters, step
 
 
+class DataStorageLocation(str, Enum):
+    """DataStorageLocation enum of valid locations.
+
+    Args:
+        str: Inherits from str class
+        Enum: Inherits from Enum class
+    """
+
+    AZURE = "azure"
+    LOCAL = "local"
+
+
 class SaveDataParameters(BaseParameters):
     """Save Data parameters."""
 
-    # Path to the data folder to save the data within the Azure Blob Storage container
+    # Location of data store
+    data_location = DataStorageLocation.LOCAL
+
+    # Path to the data folder (Local or Azure)
     data_base_dir: str = "raw_scraped_data"
 
-    # Name of the Azure Blob Storage container
+    # Name of the Azure Blob Storage container, only required if using Azure Storage
     container: str = "scraped-data-store"
 
 
@@ -44,22 +60,48 @@ def azure_upload_df(
         blob_client.upload_blob(output, blob_type="BlockBlob")
 
 
+def save_df_local(dataframe: pd.DataFrame, data_path: str, filename: str) -> None:
+    """Upload Pandas DataFrame as a csv to Azure Blob Storage container.
+
+    Args:
+        dataframe (pd.DataFrame): the DataFrame object
+        data_path (str): the path to save the data within the local storage
+        filename (str): the filename to use for the blob
+    """
+    if len(dataframe):
+        upload_file_dir = os.path.join("data", data_path)
+
+        # Create directories
+        if not os.path.exists(upload_file_dir):
+            os.makedirs(upload_file_dir)
+
+        upload_file_path = os.path.join(upload_file_dir, f"{filename}.csv")
+        output = dataframe.to_csv(index=False, encoding="utf-8")
+
+        with open(upload_file_path, "w") as f:
+            f.write(output)
+
+
 @step
 def save_data(
     nhs_data_scraped: pd.DataFrame,
     mind_data_scraped: pd.DataFrame,
     params: SaveDataParameters,
 ) -> None:
-    """Save data as CSVs and push to storage bucket.
+    """Save data as CSVs and save to specified location.
 
     Args:
-        nhs_data_scraped (pd.DataFrame): NHS data to push.
-        mind_data_scraped (pd.DataFrame): Mind data to push.
+        nhs_data_scraped (pd.DataFrame): NHS data to save.
+        mind_data_scraped (pd.DataFrame): Mind data to save.
         params (SaveDataParameters): ZenML step parameters.
     """
     # Add DateTime directory to where the data will be saved
     current_datetime = datetime.now().strftime(DATE_TIME_FORMAT)
     data_base_dir = os.path.join(params.data_base_dir, str(current_datetime))
 
-    azure_upload_df(params.container, nhs_data_scraped, data_base_dir, "nhs")
-    azure_upload_df(params.container, mind_data_scraped, data_base_dir, "mind")
+    if params.data_location == DataStorageLocation.AZURE:
+        azure_upload_df(params.container, nhs_data_scraped, data_base_dir, "nhs")
+        azure_upload_df(params.container, mind_data_scraped, data_base_dir, "mind")
+    elif params.data_location == DataStorageLocation.LOCAL:
+        save_df_local(nhs_data_scraped, data_base_dir, "nhs")
+        save_df_local(mind_data_scraped, data_base_dir, "mind")
--- tests/test_steps/test_save_data_step/test_save_data_step.py
+++ tests/test_steps/test_save_data_step/test_save_data_step.py
@@ -1,14 +1,20 @@
 """Tests for save_data_step."""
+import os
+import tempfile
 from contextlib import nullcontext as does_not_raise
+from typing import Iterator
 from unittest.mock import MagicMock, call, patch
 
 import pandas as pd
 import pytest
 from azure.storage.blob import BlobClient
+from freezegun import freeze_time
 from steps.save_data.save_data_step import (
+    DataStorageLocation,
     SaveDataParameters,
     azure_upload_df,
     save_data,
+    save_df_local,
 )
 from utils.terraform_utils import TerraformVariables
 
@@ -29,6 +35,26 @@ def mock_terraform_variables() -> TerraformVariables:
         yield mock_terraform_variables
 
 
+@pytest.fixture
+def mock_local_testing_directory() -> Iterator[str]:
+    """A fixture for creating and removing temporary test directory for storing and moving files.
+
+    Yields:
+        str: a path to temporary directory for storing and moving files from tests.
+    """
+    temp_dir = tempfile.TemporaryDirectory()
+    original_working_directory = (
+        os.getcwd()
+    )  # save in case a test changes to temp directory which will be deleted
+
+    # tests are executed at this point
+    yield temp_dir.name
+
+    # delete temp folder
+    os.chdir(original_working_directory)  # restore original working directory
+    temp_dir.cleanup()
+
+
 @pytest.fixture()
 def mock_blob_client() -> BlobClient:
     """Pytest fixture for mocking an Azure blob client and connection string.
@@ -44,6 +70,20 @@ def mock_blob_client() -> BlobClient:
         mock_blob_client = mock_container_client.get_blob_client.return_value
 
         yield mock_blob_client
+
+
+@pytest.fixture(autouse=True)
+def mock_save_data_parameters() -> SaveDataParameters:
+    """Mock step parameters.
+
+    Returns:
+        SaveDataParameters: Step parameters.
+    """
+    params = SaveDataParameters()
+    params.data_location = DataStorageLocation.AZURE
+    params.data_base_dir = "raw_scraped_data"
+    params.container = "blob_storage_container"
+    yield params
 
 
 @pytest.fixture()
@@ -81,20 +121,22 @@ def test_azure_upload_df(mock_blob_client: BlobClient, nhs_dataframe: pd.DataFra
     )
 
 
-def test_save_data_step(
+def test_save_data_step_azure(
     mock_blob_client: BlobClient,
     nhs_dataframe: pd.DataFrame,
     mind_dataframe: pd.DataFrame,
+    mock_save_data_parameters: SaveDataParameters,
 ):
-    """Test save data step works as expected.
+    """Test save data step works as expected using Azure tools.
 
     Args:
         mock_blob_client (BlobClient): Mocked Azure blob client
         nhs_dataframe (pd.DataFrame): Mocked "NHS" data in dataframe
         mind_dataframe (pd.DataFrame): Mocked "Mind" data in dataframe
+        mock_save_data_parameters (SaveDataParameters): Mocked step parameters
     """
     with does_not_raise():
-        save_data.entrypoint(nhs_dataframe, mind_dataframe, SaveDataParameters())
+        save_data.entrypoint(nhs_dataframe, mind_dataframe, mock_save_data_parameters)
 
     calls = [
         call("NHS1,NHS2\n1,3\n2,4\n", blob_type="BlockBlob"),
@@ -102,6 +144,56 @@ def test_save_data_step(
     ]
 
     mock_blob_client.upload_blob.assert_has_calls(calls)
+
+
+@freeze_time("1066-10-14")
+def test_save_data_step_local(
+    nhs_dataframe: pd.DataFrame,
+    mind_dataframe: pd.DataFrame,
+    mock_save_data_parameters: SaveDataParameters,
+    mock_local_testing_directory: str,
+):
+    """Test save data step works as expected locally.
+
+    Args:
+        nhs_dataframe (pd.DataFrame): Mocked "NHS" data in dataframe
+        mind_dataframe (pd.DataFrame): Mocked "Mind" data in dataframe
+        mock_save_data_parameters (SaveDataParameters): Mocked step parameters
+        mock_local_testing_directory (str): Location of temporary local directory for testing
+    """
+    os.chdir(mock_local_testing_directory)
+    mock_save_data_parameters.data_location = DataStorageLocation.LOCAL
+
+    with does_not_raise():
+        save_data.entrypoint(nhs_dataframe, mind_dataframe, mock_save_data_parameters)
+
+    with open(
+        os.path.join(
+            mock_local_testing_directory,
+            "data",
+            mock_save_data_parameters.data_base_dir,
+            "1066_10_14-00_00_00",
+            "nhs.csv",
+        )
+    ) as f:
+        assert f.read() == "NHS1,NHS2\n1,3\n2,4\n"
+
+
+def test_save_df_local(nhs_dataframe: pd.DataFrame, mock_local_testing_directory: str):
+    """Test local save dataframe function.
+
+    Args:
+        nhs_dataframe (pd.DataFrame): Mocked "NHS" data in dataframe
+        mock_local_testing_directory (str): Location of temporary local directory for testing
+    """
+    os.chdir(mock_local_testing_directory)
+    save_df_local(nhs_dataframe, "raw_scraped_data", "nhs")
+    with open(
+        os.path.join(
+            mock_local_testing_directory, "data", "raw_scraped_data", "nhs.csv"
+        )
+    ) as f:
+        assert f.read() == "NHS1,NHS2\n1,3\n2,4\n"
 
 
 def test_empty_dataframe_not_uploaded(mock_blob_client: BlobClient):
