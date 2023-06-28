d9c48caf9c213b5da45f1627c773ff43655f2cdd
Chris <32800386+Christopher-Norman@users.noreply.github.com>
2023-06-28 13:31:26 +0100

    [MIN-22] Remove references and use of save_data step (#12)
    * Remove save_data step from pipeline and any references
    
    * Add logging to stop precommit hooks from removing variable assignment required for artifact storage

--- pipelines/data_scraping_pipeline/data_scraping_pipeline.py
+++ pipelines/data_scraping_pipeline/data_scraping_pipeline.py
@@ -10,16 +10,15 @@ logger = get_logger(__name__)
 def data_scraping_pipeline(
     scrape_nhs_data: BaseStep,
     scrape_mind_data: BaseStep,
-    save_data: BaseStep,
 ) -> None:
     """The data scraping pipeline.
 
     Args:
         scrape_nhs_data: This step should scrape data from NHS Mental health conditions page.
         scrape_mind_data: This step should scrape data from the Mind Types of mental health problems page.
-        save_data: This step should save the scraped data as csv as push it to a storage bucket.
     """
     nhs_data = scrape_nhs_data()
     mind_data = scrape_mind_data()
 
-    save_data(nhs_data, mind_data)
+    logger.info(f"'NHS' dataset has {len(nhs_data.index)} rows.")
+    logger.info(f"'Mind' dataset has {len(mind_data.index)} rows.")
