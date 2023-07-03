f48cf2aa01c9280ef25cbb010ea8b44779bde6d2
Jonathan Carlton <jonathan.carlton@hotmail.com>
2023-07-03 15:11:42 +0100

    MIN-24 added functionality to the fetch_model step

--- steps/deployment_steps/fetch_model_step/fetch_model_step.py
+++ steps/deployment_steps/fetch_model_step/fetch_model_step.py
@@ -1,11 +1,37 @@
 """Fetch the FLAN base model and tokenizer from HuggingFace using Langchain."""
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizerBase,
+)
 from zenml import step
+from zenml.logger import get_logger
+from zenml.steps import Output
+
+logger = get_logger(__name__)
 
 
 @step
-def fetch_model() -> None:
-    """Fetch the FLAN base model and tokenizer from HuggingFace using Langchain.
+def fetch_model(
+    model_name: str,
+) -> Output(model=PreTrainedModel, tokenizers=PreTrainedTokenizerBase):  # type: ignore
+    """A step to fetch a model and tokenizer (specified by model_name) from the HuggingFace Hub.
 
-    This step should return the model and tokenizer fetched from HuggingFace.
+    Args:
+        model_name: the name of the model to fetch from the hub.
+
+    Returns:
+        PreTrainedModel: the model.
+        PreTrainedTokenizerBase: the tokenizer for the model
     """
-    ...
+    logger.info(
+        f"Fetching the model '{model_name}' and tokenizer from the HuggingFace Hub"
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    logger.info("Model and tokenizer loaded")
+
+    return model, tokenizer
---
+++ tests/test_steps/test_deployment_steps/test_fetch_model_step.py
@@ -0,0 +1,36 @@
+"""Tests for the fetch_model step in the deployment pipeline."""
+from unittest.mock import patch
+
+import pytest
+from steps.deployment_steps import fetch_model
+from transformers import (
+    PretrainedConfig,
+    PreTrainedModel,
+    PreTrainedTokenizerBase,
+)
+
+FUNCTION_LOCATION_PREFIX = "steps.deployment_steps.fetch_model_step.fetch_model_step"
+
+
+@pytest.fixture(autouse=True)
+def model_name() -> str:
+    """A fixture to define a mock `model_name` parameter for the `from_pretrained` function.
+
+    Returns:
+        str: the mocked model_name
+    """
+    return "fuzzylabs-llm-base"
+
+
+def test_fetch_model_expected():
+    """Test whether the fetch_model step returns the expected objects, a model and a tokenizer."""
+    with patch(f"{FUNCTION_LOCATION_PREFIX}.AutoTokenizer") as mock_tokenizer, patch(
+        f"{FUNCTION_LOCATION_PREFIX}.AutoModelForSeq2SeqLM"
+    ) as mock_model:
+        mock_model.from_pretrained.return_value = PreTrainedModel(PretrainedConfig())
+        mock_tokenizer.from_pretrained.return_value = PreTrainedTokenizerBase()
+
+        model, tokenizer = fetch_model(model_name=model_name)
+
+        assert isinstance(model, PreTrainedModel)
+        assert isinstance(tokenizer, PreTrainedTokenizerBase)
