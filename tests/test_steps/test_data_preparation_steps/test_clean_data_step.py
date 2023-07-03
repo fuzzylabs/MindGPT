9fd85acad8539953f7e1a4b2797358eb7d262cf8
Josh <josh.kirsopp@gmail.com>
2023-07-03 15:47:50 +0100

    update tests, remove comment on step decorator

--- tests/test_steps/test_data_preparation_steps/test_clean_data_step.py
+++ tests/test_steps/test_data_preparation_steps/test_clean_data_step.py
@@ -1,10 +1,8 @@
-from unittest.mock import MagicMock
-
+"""Test the data cleaning functionality."""
 import pandas as pd
 import pytest
-from pandas.testing import assert_frame_equal
 from steps.data_preparation_steps import clean_data
-from steps.data_preparation_steps.clean_data_step.clean_data_step import remove_punctuation, reformat
+
 
 @pytest.fixture
 def scraped_data_fixture() -> pd.DataFrame:
@@ -20,51 +18,21 @@ def scraped_data_fixture() -> pd.DataFrame:
             "https://www.link1.com",
             "https://www.link2.com",
             "https://www.link3.com",
-            "https://www.link3.com"
-        ]
+            "https://www.link3.com",
+        ],
     }
-    return pd.DataFrame(data)
-
-
-@pytest.fixture
-def cleaned_data_fixture() -> pd.DataFrame:
-    """Fixture corresponding to cleaned data from the mocked_scraped_data fixture."""
-
-    data = {
-        "sentences": [
-            "this is some scraped data it has some really wild spacing",
-            "it also has some bizzare casing"
-            "and lots of ridiculous punctuation"
-            "but otherwise, its not so bad"
-        ]
-    }
-
     return pd.DataFrame(data)
 
 
 def test_clean_data(scraped_data_fixture):
+    """Run a short test on the fake scraped data."""
     cleaned_data = clean_data(scraped_data_fixture)
     for idx in cleaned_data.index:
-        assert cleaned_data.loc[idx, "sentences"].lower() == cleaned_data.loc[idx, "sentences"]
-        assert "\n" not in cleaned_data.loc[idx, "sentences"]
-        assert cleaned_data.loc[idx, "sentences"].strip() == cleaned_data.loc[idx, "sentences"]
-        assert "\xa0" not in cleaned_data.loc[idx, "sentences"]
-        assert "  " not in cleaned_data.loc[idx, "sentences"]
-
-
-def test_remove_punctuation():
-    assert (
-        remove_punctuation(
-            "! this? string. should, ,,+contain, no =punctuation: because -–that is the rule+=():';%&_"
-        ) == " this string should contain no punctuation because that is the rule"
-    )
-    assert (
-        remove_punctuation("this string exists because python '") == "this string exists because python "
-    )
-    assert (
-        remove_punctuation('so does this one "') == "so does this one "
-    )
-    assert remove_punctuation("either/or") == "either or"
-
-def test_reformat(scraped_data_fixture, cleaned_data_fixture):
-    pass
+        assert "\n" not in cleaned_data.loc[idx, "text_scraped"]
+        assert (
+            cleaned_data.loc[idx, "text_scraped"].strip()
+            == cleaned_data.loc[idx, "text_scraped"]
+        )
+        assert "\xa0" not in cleaned_data.loc[idx, "text_scraped"]
+        assert "  " not in cleaned_data.loc[idx, "text_scraped"]
+    assert len(cleaned_data) == len(scraped_data_fixture) - 1
