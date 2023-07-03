a2645a88aec0b814d474205cfff2736872fbb019
Josh <josh.kirsopp@gmail.com>
2023-07-03 15:20:33 +0100

    docstrings for inner functions.

--- steps/data_preparation_steps/clean_data_step/clean_data_step.py
+++ steps/data_preparation_steps/clean_data_step/clean_data_step.py
@@ -20,32 +20,74 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
         The cleaned data in the new format described above.
     """
     data = data.dropna().copy()
-    data = data.drop_duplicates()
 
     def remove_new_line(s: str) -> str:
         """Remove new line characters.
 
         Args:
             s: A string.
+
+        Returns:
+            The string but without any new line characters.
         """
         return s.replace("\n", " ")
 
     def lower_case(s: str) -> str:
+        """Make the string lowercase.
+
+        Args:
+            s: A string.
+
+        Returns:
+            Lowercase version of the s arg.
+        """
         return s.lower()
 
     def strip_string(s: str) -> str:
+        """Strip the string.
+
+        Args:
+            s: A string.
+
+        Returns:
+            Stripped version of the s arg.
+        """
         return s.strip()
 
     def remove_nbsp(s: str) -> str:
+        r"""Remove non-black spaces from the string.
+
+        Args:
+            s: A string.
+
+        Returns:
+            The s arg but with no \xa0 present.
+        """
         return s.replace("\xa0", " ")
 
     def insert_space_between_numbers_and_letters(s: str) -> str:
+        """Insert a space anywhere there's a number and a letter next to each other.
+
+        Args:
+            s: A string.
+
+        Returns:
+            The s arg but with spaces inserted anywhere a letter and number are adjacent.
+        """
         regex = "(?<=[a-zA-Z])(?=\\d)|(?<=\\d)(?=[a-zA-Z])"
         subst = " "
         result = re.sub(regex, subst, s, 0)
         return result
 
     def contract_white_space(s: str) -> str:
+        """Contract multiple white spaces into one.
+
+        Args:
+            s: A string.
+
+        Returns:
+            A string containing no consecutive white space.
+        """
         return re.sub(" +", " ", s)
 
     data["text_scraped"] = data["text_scraped"].map(remove_new_line)
