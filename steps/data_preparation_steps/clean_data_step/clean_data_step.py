407ce49671e6cf2841f322ab055dbf425aab30bb
Josh <josh.kirsopp@gmail.com>
2023-07-03 09:51:10 +0100

    file header str

--- steps/data_preparation_steps/clean_data_step/clean_data_step.py
+++ steps/data_preparation_steps/clean_data_step/clean_data_step.py
@@ -1,4 +1,4 @@
-from zenml.steps import step
+"""Clean the scraped data."""
 import pandas as pd
 
 
@@ -8,10 +8,11 @@ def reformat(df: pd.DataFrame) -> pd.DataFrame:
     Args:
         df (pd.DataFrame): A pandas dataframe holding the scraped data.
 
-    Returns: A reformatted pandas DataFrame.
+    Returns:
+        A reformatted pandas DataFrame.
     """
     sentences = []
-    df['text_scraped'].map(lambda s: sentences.extend(s.split('.')))
+    df["text_scraped"].map(lambda s: sentences.extend(s.split(".")))
     return pd.DataFrame({"sentences": sentences})
 
 
@@ -23,7 +24,26 @@ def remove_punctuation(data_string: str) -> str:
 
     Returns: The string but with punctuation removed.
     """
-    punc_nospace = {"!", "?", ".", ",", "-", "–", "+", "=", "(", ")", ":", "'", ";", '"', "%", "&", "*", "_"}
+    punc_nospace = {
+        "!",
+        "?",
+        ".",
+        ",",
+        "-",
+        "–",
+        "+",
+        "=",
+        "(",
+        ")",
+        ":",
+        "'",
+        ";",
+        '"',
+        "%",
+        "&",
+        "*",
+        "_",
+    }
     for p in punc_nospace:
         data_string = data_string.replace(p, "")
 
@@ -50,24 +70,35 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     """
     data = data.dropna().copy()
     data = data.drop_duplicates()
-    remove_new_line = lambda s: s.replace("\n", " ")
-    lower_case = lambda s: s.lower()
-    strip_string = lambda s: s.strip()
-    remove_nbsp = lambda s: s.replace("\xa0", " ")
-    data['text_scraped'] = data['text_scraped'].map(remove_new_line)
-    data['text_scraped'] = data['text_scraped'].map(lower_case)
-    data['text_scraped'] = data['text_scraped'].map(strip_string)
-    data['text_scraped'] = data['text_scraped'].map(remove_nbsp)
+
+    def remove_new_line(s: str) -> str:
+        return s.replace("\n", " ")
+
+    def lower_case(s: str) -> str:
+        return s.lower()
+
+    def strip_string(s: str) -> str:
+        return s.strip()
+
+    def remove_nbsp(s: str) -> str:
+        return s.replace("\xa0", " ")
+
+    data["text_scraped"] = data["text_scraped"].map(remove_new_line)
+    data["text_scraped"] = data["text_scraped"].map(lower_case)
+    data["text_scraped"] = data["text_scraped"].map(strip_string)
+    data["text_scraped"] = data["text_scraped"].map(remove_nbsp)
 
     data = reformat(data)
 
     # now text is split into sentences, remove punc
-    data['sentences'] = data['sentences'].map(remove_punctuation)
+    data["sentences"] = data["sentences"].map(remove_punctuation)
 
-    remove_double_spaces = lambda s: s.replace("  ", " ")
-    data['sentences'] = data['sentences'].map(remove_double_spaces)
+    def remove_double_spaces(s: str) -> str:
+        return s.replace("  ", " ")
 
-    data['sentences'] = data['sentences'].map(strip_string)
+    data["sentences"] = data["sentences"].map(remove_double_spaces)
+
+    data["sentences"] = data["sentences"].map(strip_string)
 
     # drop empty strings
     data = data.drop(data[data.sentences == ""].index)
