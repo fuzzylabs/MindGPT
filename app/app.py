088a8e20c509a9d93f20fe21f4e1e57c80bfcb69
Chris <chris@fuzzylabs.ai>
2023-07-06 10:40:04 +0100

    Clean up app and add page title and icon

--- app/app.py
+++ app/app.py
@@ -1,48 +1,66 @@
-import streamlit as st
-import random
-import time
+"""MindGPT Streamlit app."""
+import json
 import os
+import time
+from typing import Any, Dict, List, Optional
 
-from typing import List, Dict
+import requests
+import streamlit as st
 
-PIPELINE_NAME = "llm_deployment_pipeline"
+PIPELINE_NAME = "deployment_pipeline"
 PIPELINE_STEP = "deploy_model"
-MODEL_NAME = "seldon-llm-custom-model"
+MODEL_NAME = "placeholder"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+st.set_page_config(
+    page_title="MindGPT",
+    page_icon="🧠",
+    layout="wide",
+)
+
+st.title("MindGPT 🧠")
+st.session_state.error_placeholder = st.empty()
 
 
 @st.cache_data
-def _get_prediction_endpoint():
+def _get_prediction_endpoint() -> Optional[str]:
     """Get the endpoint for the currently deployed LLM model.
 
     Returns:
-        str: the url endpoint.
+        Optional[str]: the url endpoint if it exists and is valid, None otherwise.
     """
-    ...
+    return None
 
 
-def _create_payload(messages: List[Dict[str, str]]) -> dict:
+def _create_payload(
+    messages: List[Dict[str, str]]
+) -> Dict[str, Dict[str, List[Dict[str, str]]]]:
     """Create a payload from the user input to send to the LLM model.
 
     Args:
-        input_text (str): Input text to summarize.
+        messages (List[Dict[str, str]]): List of previous messages from both the AI and user.
 
     Returns:
-        dict: the payload to send in the correct format.
+        Dict[str, Dict[str, List[Dict[str, str]]]]: the payload to send in the correct format.
     """
-    input_text = " ".join(f"{x.get('role', '')}: {x.get('content', '')}" for x in messages)
+    # We currently just append all messages, this could be improved to append a summary of the conversation to the start of the current user message.
+    input_text = " ".join(
+        f"{x.get('role', '')}: {x.get('content', '')}" for x in messages
+    )
     return {"data": {"ndarray": [{"text": str(input_text)}]}}
 
 
-def _get_predictions(prediction_endpoint: str, payload: dict) -> dict:
+def _get_predictions(
+    prediction_endpoint: str, payload: Dict[str, Dict[str, List[Dict[str, str]]]]
+) -> Dict[Any, Any]:
     """Using the prediction endpont and payload, make a prediction request to the deployed model.
 
     Args:
         prediction_endpoint (str): the url endpoint.
-        payload (dict): the payload to send to the model.
+        payload (Dict[str, Dict[str, List[Dict[str, str]]]]): the payload to send to the model.
 
     Returns:
-        dict: the predictions from the model.
+        Dict[Any, Any]: the predictions from the model.
     """
     response = requests.post(
         url=prediction_endpoint,
@@ -52,55 +70,64 @@ def _get_predictions(prediction_endpoint: str, payload: dict) -> dict:
     return json.loads(response.text)["jsonData"]["predictions"][0]
 
 
-def fetch_summary(seldon_url: str, txt: str) -> str:
-    """Query seldon endpoint to fetch the summary.
+def fetch_summary(prediction_endpoint: str, messages: List[Dict[str, str]]) -> str:
+    """Query endpoint to fetch the summary.
 
     Args:
-        seldon_url (str): Seldon endpoint
-        txt (str): Input text to summarize
+        prediction_endpoint (str): Prediction endpoint.
+        messages (List[Dict[str, str]]): List of previous messages from both the AI and user.
 
     Returns:
-        str: Summarized text
+        str: Summarized text.
     """
-    with st.spinner("Applying LLM Magic..."):
-        payload = _create_payload(txt)
-        summary_txt = _get_predictions(seldon_url, payload)
+    with st.spinner("Loading response..."):
+        payload = _create_payload(messages)
+        summary_txt = _get_predictions(prediction_endpoint, payload)
     return summary_txt
 
-st.title("MindGPT")
-if hasattr(st.session_state, "messages"):
-    st.text(_create_payload(st.session_state.messages))
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+def main() -> None:
+    """Main streamlit app function."""
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-if prompt := st.chat_input("Enter a question"):
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    if prompt := st.chat_input("Enter a question"):
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-with st.chat_message("assistant"):
-    message_placeholder = st.empty()
-    full_response = ""
-    assistant_response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    # Simulate stream of response with milliseconds delay
-    for chunk in assistant_response.split():
-        full_response += chunk + " "
-        time.sleep(0.05)
-        # Add a blinking cursor to simulate typing
-        message_placeholder.markdown(full_response + "▌")
-    message_placeholder.markdown(full_response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": full_response})
+        prediction_endpoint = _get_prediction_endpoint()
+
+        if prediction_endpoint is None:
+            st.session_state.error_placeholder.error(
+                "MindGPT is not reachable, please try again later.", icon="🚨"
+            )
+        else:
+            with st.chat_message("assistant"):
+                message_placeholder = st.empty()
+                full_response = ""
+                assistant_response = fetch_summary(
+                    prediction_endpoint, st.session_state.messages
+                )
+                # Simulate stream of response with milliseconds delay
+                for chunk in assistant_response.split():
+                    full_response += chunk + " "
+                    time.sleep(0.05)
+                    # Add a blinking cursor to simulate typing
+                    message_placeholder.markdown(full_response + "▌")
+                message_placeholder.markdown(full_response)
+
+                # Add assistant response to chat history
+                st.session_state.messages.append(
+                    {"role": "assistant", "content": full_response}
+                )
+
+
+if __name__ == "__main__":
+    main()
