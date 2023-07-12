14e1675d5871e11df7a0ff8584a4e8ac40872d81
Jonathan Carlton <jonathan.carlton@hotmail.com>
2023-07-12 14:24:17 +0100

    MIN-37 updated load_data + tests prior to full DVC integration

---
+++ tests/test_steps/test_data_preparation_steps/conftest.py
@@ -0,0 +1,46 @@
+"""Conftest for the data preparation steps."""
+from typing import Tuple
+from unittest.mock import MagicMock, patch
+
+import pytest
+from pandas import DataFrame
+
+LOAD_DATA_STEP = "steps.data_preparation_steps.load_data_step.load_data_step"
+
+
+@pytest.fixture
+def sample_scraping_data() -> Tuple[DataFrame, DataFrame]:
+    """Mocked data to emulate the result of the scraping pipeline.
+
+    Returns:
+        Tuple[DataFrame, DataFrame]: the two mocked dataframes with toy data.
+    """
+    return (
+        DataFrame(
+            {
+                "text_scraped": ["sample text", "more sample text"],
+                "url": ["www.mind.org.uk", "www.mind.org.uk"],
+            }
+        ),
+        DataFrame(
+            {
+                "text_scraped": ["additional text", "more additional text"],
+                "url": ["www.nhs.uk", "www.nhs.uk"],
+            }
+        ),
+    )
+
+
+@pytest.fixture
+def mocked_read_data(sample_scraping_data: Tuple[DataFrame, DataFrame]) -> MagicMock:
+    """Mocked _read_data function from the load_data step.
+
+    Args:
+        sample_scraping_data: sample data which is used as the return value of the mocked function.
+
+    Yields:
+        MagicMock: the mocked function.
+    """
+    with patch(f"{LOAD_DATA_STEP}._read_data") as read_data:
+        read_data.return_value = sample_scraping_data
+        yield read_data
