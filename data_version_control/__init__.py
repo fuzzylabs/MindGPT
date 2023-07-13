6851189860c712d1532a1d7708b9c171e833d199
Josh <josh.kirsopp@gmail.com>
2023-07-13 15:41:42 +0100

    various simple dvc functions.

--- data_version_control/__init__.py
+++ data_version_control/__init__.py
@@ -1,1 +1,17 @@
 """DVC module initialiser."""
+
+from .data_version_control import (
+    add_csv_files_to_dvc,
+    add_dvc_files_to_git,
+    checkout_data_files,
+    pull_data,
+    push_data,
+)
+
+__all__ = [
+    "pull_data",
+    "push_data",
+    "add_csv_files_to_dvc",
+    "add_dvc_files_to_git",
+    "checkout_data_files",
+]
--- data_version_control/data_version_control.py
+++ data_version_control/data_version_control.py
@@ -1,1 +1,29 @@
 """Data version control tools for interacting with DVC CLI."""
+import subprocess as sp
+
+from config import ROOT_DIR
+
+
+def pull_data() -> None:
+    """Pull the most recently pushed data from the storage bucket."""
+    sp.run("dvc pull", shell=True, cwd=ROOT_DIR)
+
+
+def push_data() -> None:
+    """Push the current data to the bucket."""
+    sp.run("dvc push", shell=True, cwd=ROOT_DIR)
+
+
+def add_csv_files_to_dvc() -> None:
+    """Add the current csv files to dvc."""
+    sp.run("dvc add data/*.csv", shell=True, cwd=ROOT_DIR)
+
+
+def add_dvc_files_to_git() -> None:
+    """Add *.dvc files in the data/ directory to git."""
+    sp.run("git add data/*.dvc", shell=True, cwd=ROOT_DIR)
+
+
+def checkout_data_files() -> None:
+    """Checkout the data files consistent with the hash in the current .dvc files in the data/ directory."""
+    sp.run("dvc checkout", shell=True, cwd=ROOT_DIR)
