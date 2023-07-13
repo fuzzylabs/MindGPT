f6c3286affd3a009237b9c4d5189046e45bcc3d3
Josh <josh.kirsopp@gmail.com>
2023-07-13 15:50:41 +0100

    dvc functionality

--- data_version_control/__init__.py
+++ data_version_control/__init__.py
@@ -4,8 +4,11 @@ from .data_version_control import (
     add_csv_files_to_dvc,
     add_dvc_files_to_git,
     checkout_data_files,
+    commit_new_dcv_changes,
     pull_data,
+    pull_data_consistent_with_git,
     push_data,
+    version_new_data,
 )
 
 __all__ = [
@@ -14,4 +17,7 @@ __all__ = [
     "add_csv_files_to_dvc",
     "add_dvc_files_to_git",
     "checkout_data_files",
+    "commit_new_dcv_changes",
+    "version_new_data",
+    "pull_data_consistent_with_git",
 ]
--- data_version_control/data_version_control.py
+++ data_version_control/data_version_control.py
@@ -27,3 +27,20 @@ def add_dvc_files_to_git() -> None:
 def checkout_data_files() -> None:
     """Checkout the data files consistent with the hash in the current .dvc files in the data/ directory."""
     sp.run("dvc checkout", shell=True, cwd=ROOT_DIR)
+
+
+def commit_new_dcv_changes() -> None:
+    """Commit the changes to the dcv files to git."""
+    sp.run('git commit -m "Update the scraped data"')
+
+
+def version_new_data() -> None:
+    """Version new data."""
+    add_csv_files_to_dvc()
+    add_dvc_files_to_git()
+    commit_new_dcv_changes()
+
+
+def pull_data_consistent_with_git() -> None:
+    """Checkout the data consistent with the dcv files in the current commit."""
+    sp.run("dvc checkout", shell=True, cwd=ROOT_DIR)
