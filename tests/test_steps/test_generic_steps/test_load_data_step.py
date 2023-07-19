b3b16e8b84f2ed033b4cec26c1785266fdb6ebcb
Chris <chris@fuzzylabs.ai>
2023-07-19 15:03:01 +0100

    Migrate mocks to fixtures

--- tests/test_steps/test_generic_steps/test_load_data_step.py
+++ tests/test_steps/test_generic_steps/test_load_data_step.py
@@ -1,6 +1,7 @@
 """Test load data step."""
 import os
 from typing import Tuple
+from unittest import mock
 from unittest.mock import patch
 
 import pytest
@@ -9,14 +10,61 @@ from pandas.testing import assert_frame_equal
 from steps.generic_steps import load_data
 
 
+@pytest.fixture
+def mock_git_checkout_folder():
+    """Mock git checkout folder function.
+
+    Yields:
+        git_checkout_folder (mock.MagicMock): Mocked git_checkout_folder function.
+    """
+    with patch(
+        "steps.generic_steps.load_data_step.load_data_step.git_checkout_folder"
+    ) as git_checkout_folder:
+        yield git_checkout_folder
+
+
+@pytest.fixture
+def mock_pull_data():
+    """Mock pull_data function.
+
+    Yields:
+        pull_data (mock.MagicMock): Mocked pull_data function.
+    """
+    with patch(
+        "steps.generic_steps.load_data_step.load_data_step.pull_data"
+    ) as pull_data:
+        yield pull_data
+
+
+@pytest.fixture(autouse=True)
+def mock_project_root_directory(directory_for_testing: str):
+    """Mocked PROJECT_ROOT_DIR constant.
+
+    Args:
+        directory_for_testing (str): Mocked local directory.
+
+    Yields:
+        mock_project_root_dir (mock.MagicMock): Mocked directory.
+    """
+    with mock.patch(
+        "utils.data_version_control.PROJECT_ROOT_DIR", directory_for_testing
+    ) as mock_project_root_dir:
+        yield mock_project_root_dir
+
+
 def test_load_data(
-    directory_for_testing: str, sample_scraping_data: Tuple[DataFrame, DataFrame]
+    directory_for_testing: str,
+    sample_scraping_data: Tuple[DataFrame, DataFrame],
+    mock_git_checkout_folder: mock.MagicMock,
+    mock_pull_data: mock.MagicMock,
 ):
     """Tests that the correct functions are called when loading data.
 
     Args:
         directory_for_testing (str): Mock testing directory.
         sample_scraping_data (Tuple[DataFrame, DataFrame]): Mock DataFrames for saving/loading.
+        mock_git_checkout_folder (mock.MagicMock): Mocked git_checkout_folder function.
+        mock_pull_data (mock.MagicMock): Mocked pull_data function.
     """
     data_location = os.path.join(directory_for_testing, "data")
     os.mkdir(data_location)
@@ -24,80 +72,59 @@ def test_load_data(
     mind.to_csv(f"{data_location}/mind_data_raw.csv", index=False)
     nhs.to_csv(f"{data_location}/nhs_data_raw.csv", index=False)
 
-    with patch(
-        "steps.generic_steps.load_data_step.load_data_step.git_checkout_folder"
-    ) as git_checkout_folder, patch(
-        "steps.generic_steps.load_data_step.load_data_step.pull_data"
-    ) as pull_data, patch(
-        "steps.generic_steps.load_data_step.load_data_step.DATA_DIR", data_location
-    ):
-        loaded_mind_df, loaded_nhs_df = load_data(
-            data_version="test", data_postfix="raw"
-        )
+    loaded_mind_df, loaded_nhs_df = load_data(data_version="test", data_postfix="raw")
 
-        git_checkout_folder.assert_called_once()
-        pull_data.assert_called_once()
-        assert_frame_equal(mind, loaded_mind_df)
-        assert_frame_equal(nhs, loaded_nhs_df)
+    mock_git_checkout_folder.assert_called_once()
+    mock_pull_data.assert_called_once()
+    assert_frame_equal(mind, loaded_mind_df)
+    assert_frame_equal(nhs, loaded_nhs_df)
 
 
 def test_load_data_raises_exception_when_data_does_not_exist(
     directory_for_testing: str,
+    mock_git_checkout_folder: mock.MagicMock,
+    mock_pull_data: mock.MagicMock,
 ):
     """Tests that an error is raised when required data files do not exist.
 
     Args:
         directory_for_testing (str): Mock testing directory.
         sample_scraping_data (Tuple[DataFrame, DataFrame]): Mock DataFrames for saving/loading.
+        mock_git_checkout_folder (mock.MagicMock): Mocked git_checkout_folder function.
+        mock_pull_data (mock.MagicMock): Mocked pull_data function.
     """
     data_location = os.path.join(directory_for_testing, "data")
     os.mkdir(data_location)
 
-    with patch(
-        "steps.generic_steps.load_data_step.load_data_step.git_checkout_folder"
-    ) as git_checkout_folder, patch(
-        "steps.generic_steps.load_data_step.load_data_step.pull_data"
-    ) as pull_data, patch(
-        "steps.generic_steps.load_data_step.load_data_step.DATA_DIR", data_location
-    ):
-        with pytest.raises(FileNotFoundError) as e:
-            _, _ = load_data(data_version="test", data_postfix="raw")
+    with pytest.raises(FileNotFoundError) as e:
+        _, _ = load_data(data_version="test", data_postfix="raw")
 
-            assert (
-                "Required CSV files do not exist in 'data' folder, ensure the previous pipeline has been run."
-                in str(e)
-            )
+        assert (
+            "Required CSV files do not exist in 'data' folder, ensure the previous pipeline has been run."
+            in str(e)
+        )
 
-        git_checkout_folder.assert_called_once()
-        pull_data.assert_called_once()
+    mock_git_checkout_folder.assert_called_once()
+    mock_pull_data.assert_called_once()
 
 
 def test_load_data_raises_exception_when_data_directory_does_not_exist(
-    directory_for_testing: str,
+    mock_git_checkout_folder: mock.MagicMock, mock_pull_data: mock.MagicMock
 ):
     """Tests that an error is raised when the data folder does not exist.
 
     Args:
-        directory_for_testing (str): Mock testing directory.
-        sample_scraping_data (Tuple[DataFrame, DataFrame]): Mock DataFrames for saving/loading.
+        mock_git_checkout_folder (mock.MagicMock): Mocked git_checkout_folder function.
+        mock_pull_data (mock.MagicMock): Mocked pull_data function.
     """
-    data_location = os.path.join(directory_for_testing, "data")
+    mock_git_checkout_folder.side_effect = FileNotFoundError(
+        "Folder with the name 'data' does not exist."
+    )
 
-    with patch(
-        "steps.generic_steps.load_data_step.load_data_step.git_checkout_folder"
-    ) as git_checkout_folder, patch(
-        "steps.generic_steps.load_data_step.load_data_step.pull_data"
-    ) as pull_data, patch(
-        "steps.generic_steps.load_data_step.load_data_step.DATA_DIR", data_location
-    ):
-        git_checkout_folder.side_effect = FileNotFoundError(
-            "Folder with the name 'data' does not exist."
-        )
+    with pytest.raises(FileNotFoundError) as e:
+        _, _ = load_data(data_version="test", data_postfix="raw")
 
-        with pytest.raises(FileNotFoundError) as e:
-            _, _ = load_data(data_version="test", data_postfix="raw")
+        assert "Folder with the name 'data' does not exist." in str(e)
 
-            assert "Folder with the name 'data' does not exist." in str(e)
-
-        git_checkout_folder.assert_called_once()
-        pull_data.assert_not_called()
+    mock_git_checkout_folder.assert_called_once()
+    mock_pull_data.assert_not_called()
