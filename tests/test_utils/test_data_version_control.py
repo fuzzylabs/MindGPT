c38f1e315aa94de112fd2d7cef3674647c2a5485
Chris <chris@fuzzylabs.ai>
2023-07-19 14:52:10 +0100

    Migrate mocks to fixtures

--- tests/test_utils/test_data_version_control.py
+++ tests/test_utils/test_data_version_control.py
@@ -49,6 +49,32 @@ def mock_project_root_directory(directory_for_testing: str):
         yield mock_project_root_dir
 
 
+@pytest.fixture
+def mock_git_tag_exists():
+    """Mock function _git_tag_exists.
+
+    Yields:
+        mock_git_tag_exists (mock.MagicMock): Mock function _git_tag_exists.
+    """
+    with mock.patch(
+        "utils.data_version_control._git_tag_exists"
+    ) as mock_git_tag_exists:
+        yield mock_git_tag_exists
+
+
+@pytest.fixture
+def mock_git_commit_hash_exists():
+    """Mock function _git_commit_hash_exists.
+
+    Yields:
+        mock_git_commit_hash_exists (mock.MagicMock): Mock function _git_commit_hash_exists.
+    """
+    with mock.patch(
+        "utils.data_version_control._git_commit_hash_exists"
+    ) as mock_git_commit_hash_exists:
+        yield mock_git_commit_hash_exists
+
+
 def test_file_not_found_error_csv(directory_for_testing):
     """Test that the add_csv_files_to_dvc function raises an error when the files don't exist."""
     test_paths = [
@@ -70,75 +96,67 @@ def test_file_not_found_error_dvc(directory_for_testing):
 
 
 def test_git_checkout_folder_raises_file_not_found_error_without_directory(
-    directory_for_testing,
-    mock_git_command,
+    mock_git_command, mock_git_tag_exists, mock_git_commit_hash_exists
 ):
     """Test the git_checkout_folder function raises a FileNotFound exception when the specified directory does not exist.
 
     Args:
-        directory_for_testing (str): Path to testing directory.
         mock_git_command (mock.MagicMock()): Mocked git command object.
+        mock_git_tag_exists (mock.MagicMock): Mock function _git_tag_exists.
+        mock_git_commit_hash_exists (mock.MagicMock): Mock function _git_commit_hash_exists.
     """
-    with mock.patch(
-        "utils.data_version_control._git_tag_exists"
-    ) as mock_git_tag_exists, mock.patch(
-        "utils.data_version_control._git_commit_hash_exists"
-    ) as mock_git_commit_hash_exists:
-        mock_git_command.return_value = mock.MagicMock()
-        mock_git_tag_exists.return_value = True
-        mock_git_commit_hash_exists.return_value = True
+    mock_git_command.return_value = mock.MagicMock()
+    mock_git_tag_exists.return_value = True
+    mock_git_commit_hash_exists.return_value = True
 
-        with pytest.raises(FileNotFoundError):
-            git_checkout_folder(tag_name="version1", folder_name="data")
+    with pytest.raises(FileNotFoundError):
+        git_checkout_folder(tag_name="version1", folder_name="data")
 
 
 def test_git_checkout_folder_raises_vaue_error_invalid_tag(
     directory_for_testing,
     mock_git_command,
+    mock_git_tag_exists,
+    mock_git_commit_hash_exists,
 ):
     """Test the git_checkout_folder function raises ValueError when the tag is invalid.
 
     Args:
         directory_for_testing (str): Path to testing directory.
         mock_git_command (mock.MagicMock()): Mocked git command object.
+        mock_git_tag_exists (mock.MagicMock): Mock function _git_tag_exists.
+        mock_git_commit_hash_exists (mock.MagicMock): Mock function _git_commit_hash_exists.
     """
     data_path = os.path.join(directory_for_testing, "data")
     os.mkdir(data_path)
-    with mock.patch(
-        "utils.data_version_control._git_tag_exists"
-    ) as mock_git_tag_exists, mock.patch(
-        "utils.data_version_control._git_commit_hash_exists"
-    ) as mock_git_commit_hash_exists:
-        mock_git_command.return_value = mock.MagicMock()
-        mock_git_tag_exists.return_value = False
-        mock_git_commit_hash_exists.return_value = True
 
-        with pytest.raises(ValueError):
-            git_checkout_folder(tag_name="version1", folder_name="data")
+    mock_git_command.return_value = mock.MagicMock()
+    mock_git_tag_exists.return_value = False
+    mock_git_commit_hash_exists.return_value = True
+
+    with pytest.raises(ValueError):
+        git_checkout_folder(tag_name="version1", folder_name="data")
 
 
 def test_git_checkout_folder_raises_vaue_error_invalid_commit(
-    directory_for_testing, mock_git_command
+    directory_for_testing, mock_git_command, mock_git_tag_exists
 ):
     """Test the git_checkout_folder function raises ValueError when the commit is invalid.
 
     Args:
         directory_for_testing (str): Path to testing directory.
         mock_git_command (mock.MagicMock()): Mocked git command object.
+        mock_git_tag_exists (mock.MagicMock): Mock function _git_tag_exists.
     """
     data_path = os.path.join(directory_for_testing, "data")
     os.mkdir(data_path)
-    with mock.patch(
-        "utils.data_version_control._git_tag_exists"
-    ) as mock_git_tag_exists, mock.patch(
-        "utils.data_version_control._git_commit_hash_exists"
-    ) as mock_git_commit_hash_exists:
-        mock_git_command.return_value = mock.MagicMock()
-        mock_git_tag_exists.return_value = True
-        mock_git_commit_hash_exists.return_value = False
 
-        with pytest.raises(ValueError):
-            git_checkout_folder(commit_hash="abcdefg", folder_name="data")
+    mock_git_command.return_value = mock.MagicMock()
+    mock_git_tag_exists.return_value = True
+    mock_git_commit_hash_exists.return_value = False
+
+    with pytest.raises(ValueError):
+        git_checkout_folder(commit_hash="abcdefg", folder_name="data")
 
 
 def test_git_tag_exists(directory_for_testing, mock_git_repo):
@@ -178,7 +196,7 @@ def test_git_tag_exists_returns_false_when_tag_does_not_exist(
     assert not _git_tag_exists("tag3")
 
 
-def test_git_commit_hash_exists(directory_for_testing, mock_git_repo):
+def test_git_commit_hash_exists(mock_git_repo):
     """Test the _git_commit_hash_exists function works as expected.
 
     Args:
@@ -192,13 +210,10 @@ def test_git_commit_hash_exists(directory_for_testing, mock_git_repo):
     assert _git_commit_hash_exists("hash1")
 
 
-def test_git_commit_hash_exists_returns_false_when_tag_does_not_exist(
-    directory_for_testing, mock_git_repo
-):
+def test_git_commit_hash_exists_returns_false_when_tag_does_not_exist(mock_git_repo):
     """Test the _git_commit_hash_exists returns false when a Git tag does not exist.
 
     Args:
-        directory_for_testing (str): Path to testing directory.
         mock_git_repo (mock.MagicMock()): Mocked git repo object.
     """
     mock_git_repo_value = mock.MagicMock()
