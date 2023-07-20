10b6e6fac297daa9da78c424804c388767eacd16
Chris <32800386+Christopher-Norman@users.noreply.github.com>
2023-07-20 09:44:08 +0100

    [MIN-49] Create load data from DVC version step (#32)

--- pipelines/data_embedding_pipeline/data_embedding_pipeline.py
+++ pipelines/data_embedding_pipeline/data_embedding_pipeline.py
@@ -1,5 +1,6 @@
 """Data embedding pipeline."""
-from steps.data_embedding_steps import embed_data, fetch_data
+from steps.data_embedding_steps import embed_data
+from steps.generic_steps import load_data
 from zenml import pipeline
 from zenml.logger import get_logger
 
@@ -11,10 +12,10 @@ def data_embedding_pipeline() -> None:
     """The data embedding pipeline.
 
     Steps:
-        fetch_data: A ZenML step which loads the data from a specified DVC data version.
+        load_data: A ZenML step which loads the data from a specified DVC data version.
         embed_data: A ZenML step which embeds the text data into vectors and pushes to the vector database.
     """
-    mind_df, nhs_df = fetch_data()
+    mind_df, nhs_df = load_data()
 
     embed_data(mind_df, collection_name="mind_data")
     embed_data(nhs_df, collection_name="nhs_data")
--- steps/data_embedding_steps/__init__.py
+++ steps/data_embedding_steps/__init__.py
@@ -1,6 +1,5 @@
 """Initialiser for data embedding steps."""
 
 from .embed_data_step.embed_data_step import embed_data
-from .fetch_data_step.fetch_data_step import fetch_data
 
-__all__ = ["embed_data", "fetch_data"]
+__all__ = ["embed_data"]
