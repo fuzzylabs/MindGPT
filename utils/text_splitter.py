a1d66d918d4ac3f9d5c3a977e9f2d062baf4f70c
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2023-08-03 19:46:49 +0530

    add spacing and remove single variables

--- utils/text_splitter.py
+++ utils/text_splitter.py
@@ -2,6 +2,9 @@
 # Modified from : https://github.com/langchain-ai/langchain/blob/37aade19da2f4c974e95d0758a796467cdccf1b1/libs/langchain/langchain/text_splitter.py
 import re
 from typing import Iterable, List, Optional
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def join_docs(docs: List[str], separator: str) -> Optional[str]:
@@ -16,10 +19,7 @@ def join_docs(docs: List[str], separator: str) -> Optional[str]:
     """
     text = separator.join(docs)
     text = text.strip()
-    if not text:
-        return None
-    else:
-        return text
+    return text if text else None
 
 
 def split_text_with_regex(text: str, separator: str) -> List[str]:
@@ -66,42 +66,44 @@ class TextSplitter:
         """
         # Combine these smaller pieces into medium size chunks to send to the LLM.
         separator_len = len(separator)
-
         docs = []
-        current_doc: List[str] = []
+        current_doc = []
         total = 0
-        for d in splits:
-            _len = len(d)
-            if (
-                total + _len + (separator_len if len(current_doc) > 0 else 0)
-                > self.chunk_size
-            ):
+
+        for split in splits:
+            _len = len(split)
+
+            total_length = total + _len + (separator_len if len(current_doc) > 0 else 0)
+
+            if total_length > self.chunk_size:
                 if total > self.chunk_size:
-                    print(
+                    logger.warning(
                         f"Created a chunk of size {total}, "
                         f"which is longer than the specified {self.chunk_size}"
                     )
+
                 if len(current_doc) > 0:
                     doc = join_docs(current_doc, separator)
                     if doc is not None:
                         docs.append(doc)
+
                     # Keep on popping if:
                     # - we have a larger chunk than in the chunk overlap
                     # - or if we still have any chunks and the length is long
+                    separator_length = separator_len if len(current_doc) > 1 else 0
                     while total > self.chunk_overlap or (
-                        total + _len + (separator_len if len(current_doc) > 0 else 0)
-                        > self.chunk_size
-                        and total > 0
+                        total_length > self.chunk_size and total > 0
                     ):
-                        total -= len(current_doc[0]) + (
-                            separator_len if len(current_doc) > 1 else 0
-                        )
+                        total -= len(current_doc[0]) + separator_length
                         current_doc = current_doc[1:]
-            current_doc.append(d)
-            total += _len + (separator_len if len(current_doc) > 1 else 0)
+            current_doc.append(split)
+            total += _len + separator_length
+
         doc = join_docs(current_doc, separator)
+
         if doc is not None:
             docs.append(doc)
+
         return docs
 
     def split_text(
@@ -118,9 +120,11 @@ class TextSplitter:
             List[str]: List of chunks
         """
         final_chunks = []
+
         # Get appropriate separator to use
         separator = separators[-1]
         new_separators = []
+
         for i, _s in enumerate(separators):
             if not _s:
                 separator = _s
@@ -131,23 +135,27 @@ class TextSplitter:
                 break
 
         splits = split_text_with_regex(text, separator)
+
         # Now go merging things, recursively splitting longer texts.
         _good_splits = []
         _separator = ""
-        for s in splits:
-            if len(s) < self.chunk_size:
-                _good_splits.append(s)
+
+        for split in splits:
+            if len(split) < self.chunk_size:
+                _good_splits.append(split)
             else:
                 if _good_splits:
                     merged_text = self.merge_splits(_good_splits, _separator)
                     final_chunks.extend(merged_text)
                     _good_splits = []
                 if not new_separators:
-                    final_chunks.append(s)
+                    final_chunks.append(split)
                 else:
-                    other_info = self.split_text(s, new_separators)
+                    other_info = self.split_text(split, new_separators)
                     final_chunks.extend(other_info)
+
         if _good_splits:
             merged_text = self.merge_splits(_good_splits, _separator)
             final_chunks.extend(merged_text)
+
         return final_chunks
